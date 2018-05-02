9ad330bee183a35552e4c8c2a857adcb2601b8ff
morrisz <morrisz@cpscentral.com>
2018-05-02 10:38:48 -0400

    gitignore

--- credentials.js
+++
@@ -1,7 +0,0 @@
-module.exports= {
-    
-    network: 'ropsten',
-    infura_api_key: '6Z8pW5MQzU4t0YIjKXVK',
-    cps_public_key: '0xd968F006E86C045B9e69754CF256955336c1a082',
-    cps_private_key: '3cfc558703d00da417b82fcfaa622b394f4705ef285f09bf7a40cd24bdc0d6c0'
-}
